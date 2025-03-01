019b97fe78027179efe8655caf2e22c15ee9355f
The-Enigma-Code <niteshchaudhary0812@gmail.com>
2025-03-01 14:36:05 +0530

    version  1.0 Using sample.json local offline data

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+// import Temp from './Components/Temp';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Temp/> */}
+    <Navbar/>
+    <News/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Temp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Temp extends Component {
+    a="nik"
+  render() {
+    return (
+      <div>
+        Hello this is {this.a}
+      </div>
+    )
+  }
+}
